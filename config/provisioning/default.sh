c8b9866f6c58d3ec77e2a48f905bca4d078db1e9
gachiforyou <gachiforyou@proton.me>
2026-02-21 02:34:14 +0900

    Update default.sh

--- config/provisioning/default.sh
+++ config/provisioning/default.sh
@@ -100,48 +100,4 @@ download_if_missing () {
   fi
 }
 
-########################################
-# 9. LTX2 19B Distilled 모델 세트
-########################################
 
-# Diffusion transformer_only
-download_if_missing \
-"$MODEL_DIR/diffusion_models/ltx-2-19b-distilled_transformer_only_bf16.safetensors" \
-"https://huggingface.co/Kijai/LTXV2_comfy/resolve/main/diffusion_models/ltx-2-19b-distilled_transformer_only_bf16.safetensors"
-
-# Full distilled checkpoint
-download_if_missing \
-"$MODEL_DIR/checkpoints/ltx-2-19b-distilled.safetensors" \
-"https://huggingface.co/Lightricks/LTX-2/resolve/main/ltx-2-19b-distilled.safetensors"
-
-# Embedding connector
-download_if_missing \
-"$MODEL_DIR/checkpoints/ltx-2-19b-embeddings_connector_distill_bf16.safetensors" \
-"https://huggingface.co/Kijai/LTXV2_comfy/resolve/main/text_encoders/ltx-2-19b-embeddings_connector_distill_bf16.safetensors"
-
-# Text encoder
-download_if_missing \
-"$MODEL_DIR/text_encoders/gemma_3_12B_it.safetensors" \
-"https://huggingface.co/Comfy-Org/ltx-2/resolve/main/split_files/text_encoders/gemma_3_12B_it.safetensors"
-
-# VAE video
-download_if_missing \
-"$MODEL_DIR/vae/LTX2_video_vae_bf16.safetensors" \
-"https://huggingface.co/Kijai/LTXV2_comfy/resolve/main/VAE/LTX2_video_vae_bf16.safetensors"
-
-# VAE audio
-download_if_missing \
-"$MODEL_DIR/vae/LTX2_audio_vae_bf16.safetensors" \
-"https://huggingface.co/Kijai/LTXV2_comfy/resolve/main/VAE/LTX2_audio_vae_bf16.safetensors"
-
-# LoRA detailer
-download_if_missing \
-"$MODEL_DIR/loras/ltx-2-19b-ic-lora-detailer.safetensors" \
-"https://huggingface.co/Lightricks/LTX-2-19b-IC-LoRA-Detailer/resolve/main/ltx-2-19b-ic-lora-detailer.safetensors"
-
-# Latent upscaler
-download_if_missing \
-"$MODEL_DIR/latent_upscale_models/ltx-2-spatial-upscaler-x2-1.0.safetensors" \
-"https://huggingface.co/Lightricks/LTX-2/resolve/main/ltx-2-spatial-upscaler-x2-1.0.safetensors"
-
-echo "🎉 모든 노드 + LTX2 19B Distilled 세팅 완료!"
